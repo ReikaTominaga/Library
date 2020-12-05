3ec7a3fed05ec137b2ab35ceda75faa7bef5d519
ReikaTominaga <vip.0x0.best7@gmail.com>
2020-12-05 12:44:13 +0900

    add new file

--- src/UserList.java
+++ src/UserList.java
@@ -1,14 +1,17 @@
-
 public class UserList {
 
 	private String[] userList = new String[100];
 
     addUser() {
 
+        System.out.println("登録する利用者名を入力してください");
+
 
     }
 
     delUser(){
+
+        System.out.println("削除する利用者名を入力してください");
 
 
     }
